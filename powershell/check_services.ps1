55a75400656e70e725b14a0b5123e0057fe74897
Chris Bull <chris.bull@opsview.com>
2024-12-11 11:09:30 +0000

    Release 2.2.48243

--- powershell/check_services.ps1
+++ powershell/check_services.ps1
@@ -26,8 +26,6 @@ http://www.opsview.com
 
 #>
 
-
-
 param(
 [alias("h")] [switch]$help,
 [string[]]$ServiceName,
@@ -36,63 +34,90 @@ param(
 [string[]]$ExcludeStatus,
 [string[]]$StartMode
 )
-$NagiosStatus = "0"
-$StatusList = [ordered]@{"OK" = "0"; "Error" = "0"; "Degraded" = "0"; "Unknown" = "0"; "Pred_Fail" = "0"; "Starting" = "0"; "Stopping" = "0"; "Service" = "0"; "Stressed" = "0"; "NonRecover" = "0"; "No_Contact" = "0"; "Lost_Comm" = "0"}
 
-if (!$StartMode)
-{
-    $StartMode = @("Boot", "System", "Auto", "Manual", "Disabled", "Unknown")
+$NagiosStatus = "0"
+$StatusList = [ordered]@{
+    "OK" = "0";
+    "Error" = "0";
+    "Degraded" = "0";
+    "Unknown" = "0";
+    "Pred_Fail" = "0";
+    "Starting" = "0";
+    "Stopping" = "0";
+    "Service" = "0";
+    "Stressed" = "0";
+    "NonRecover" = "0";
+    "No_Contact" = "0";
+    "Lost_Comm" = "0"
 }
 
-if ($help)
-{
+if ($help) {
     Get-Help $MyInvocation.MyCommand.Definition 
     exit 3
 }
 
-if ($ExcludeService -ne $null){
-    $excludeList = $ExcludeService.split(",")
+if ($ServiceName -ne $null) {
+    $serviceNameList = $ServiceName.split(",")
+} else {
+    $serviceNameList = ""
 }
 
+if ($ExcludeService -ne $null) {
+    $excludeServiceList = $ExcludeService.split(",")
+} else {
+    $excludeServiceList = ""
+}
 
+if ($ExcludeState -ne $null) {
+    $excludeStateList = $ExcludeState.split(",")
+} else {
+    $excludeStateList = ""
+}
 
-if (!$ServiceName)
-{
+if ($ExcludeStatus -ne $null) {
+    $excludeStatusList = $ExcludeStatus.split(",")
+} else {
+    $excludeStatusList = ""
+}
+
+if (!$StartMode) {
+    $startModeList = @("Boot", "System", "Auto", "Manual", "Disabled", "Unknown")
+} else {
+    $startModeList = $StartMode.split(",")
+}
+
+if (!$ServiceName) {
     Get-WmiObject win32_Service | Select-Object Name, State, Status, StartMode | Sort-Object -Property Name | ForEach-Object {
-        
+
         $wildcardMatched = $false
-        foreach ($wildcardservice in $excludeList) {
+        foreach ($wildcardservice in $excludeServiceList) {
             if($wildcardservice -match "\*") {
-                if($_.Name -like $wildcardservice){
+                if ($_.Name -like $wildcardservice) {
                     $wildcardMatched = $true
                 }
             }
         }
 
-        #Write-Output "Service: $($_.name) wilcardstatus: $wilcard"
-        
         if (!$wildcardMatched) {
-            if ($ExcludeService -notcontains $_.Name -and $ExcludeState -notcontains $_.State -and $StartMode -contains $_.StartMode -and $ExcludeStatus -notcontains $_.Status)
-            {
-                if ($_.State -ne "Running")
-                {
-                    if ($NagiosDescription)
-                    {
+            if (
+                $excludeServiceList -notcontains $_.Name -and
+                $excludeStateList -notcontains $_.State -and
+                $startModeList -contains $_.StartMode -and
+                $excludeStatusList -notcontains $_.Status
+            ) {
+                if ($_.State -ne "Running") {
+                    if ($NagiosDescription) {
                         $NagiosDescription = $NagiosDescription + ", "
                     }
                     $NagiosDescription = $NagiosDescription + $_.Name + " (" + $_.State + ", Status: " + $_.Status + ")"
                     $NagiosStatus = "2"
-                }
-                elseif ($_.State -eq "Running" -and $_.Status -ne "OK")
-                {
-                    if ($NagiosDescription)
-                    {
+                } elseif ($_.State -eq "Running" -and $_.Status -ne "OK") {
+                    if ($NagiosDescription) {
                         $NagiosDescription = $NagiosDescription + ", "
                     }
                     $NagiosDescription = $NagiosDescription + $_.Name + " (" + $_.State + ", Status: " + $_.Status + ")"
-                    if ($NagiosStatus -ne "2")
-                    {
-                    $NagiosStatus = "1"
+                    if ($NagiosStatus -ne "2") {
+                        $NagiosStatus = "1"
                     }
                 }
                 $SStatus = $_.Status -replace '\s','_'
@@ -101,48 +126,44 @@ if (!$ServiceName)
             }
         }
     }
-}
-else
-{
-    Get-WmiObject win32_Service| Select-Object Name, State, Status, StartMode | ForEach-Object {
+} else {
+    Get-WmiObject win32_Service | Select-Object Name, State, Status, StartMode | ForEach-Object {
 
-        $matchedService=$false
-        foreach ($singleService in $ServiceName){
-            if ($_.Name -like $singleService){
-                $matchedService=$true
+        $matchedService = $false
+        foreach ($singleService in $serviceNameList) {
+            if ($_.Name -like $singleService) {
+                $matchedService = $true
             }
         }
 
-        $matchedExcludeService=$false
-        foreach ($excludeService in $excludeList){
-            if ($_.Name -like $excludeService){
+        $matchedExcludeService = $false
+        foreach ($excludeService in $excludeServiceList) {
+            if ($_.Name -like $excludeService) {
                 $matchedExcludeService=$true
             }
         }
 
-
-
-        if ($matchedService -and !$matchedExcludeService -and $ExcludeService -notcontains $_.Name -and $ExcludeState -notcontains $_.State -and $StartMode -contains $_.StartMode -and $ExcludeStatus -notcontains $_.Status)
-        {
-            if ($_.State -ne "Running")
-            {
-                if ($NagiosDescription)
-                {
+        if (
+            $matchedService -and
+            !$matchedExcludeService -and
+            $excludeServiceList -notcontains $_.Name -and
+            $excludeStateList -notcontains $_.State -and
+            $startModeList -contains $_.StartMode -and
+            $excludeStatusList -notcontains $_.Status
+        ) {
+            if ($_.State -ne "Running") {
+                if ($NagiosDescription) {
                     $NagiosDescription = $NagiosDescription + ", "
                 }
                 $NagiosDescription = $NagiosDescription + $_.Name + " (" + $_.State + ", Status: " + $_.Status + ")"
                 $NagiosStatus = "2"
-            }
-            elseif ($_.State -eq "Running" -and $_.Status -ne "OK")
-            {
-                if ($NagiosDescription)
-                {
+            } elseif ($_.State -eq "Running" -and $_.Status -ne "OK") {
+                if ($NagiosDescription) {
                     $NagiosDescription = $NagiosDescription + ", "
                 }
                 $NagiosDescription = $NagiosDescription + $_.Name + " (" + $_.State + ", Status: " + $_.Status + ")"
-                if ($NagiosStatus -ne "2")
-                {
-                $NagiosStatus = "1"
+                if ($NagiosStatus -ne "2") {
+                    $NagiosStatus = "1"
                 }
             }
             $SStatus = $_.Status -replace '\s','_'
@@ -156,16 +177,11 @@ foreach ($ServiceState in $StatusList.Keys) {
     $NagiosPerfData = $NagiosPerfData + "${ServiceState}=$($StatusList.Item($ServiceState)) "
 }
 
-if ($NagiosStatus -eq "2")
-{
+if ($NagiosStatus -eq "2") {
     Write-Host "CRITICAL:" $NagiosDescription " | " $NagiosPerfData
-}
-elseif ($NagiosStatus -eq "1")
-{
+} elseif ($NagiosStatus -eq "1") {
     Write-Host "WARNING:" $NagiosDescription " | "$NagiosPerfData
-}
-else
-{
+} else {
     Write-Host "OK: All Services running | " $NagiosPerfData
 }
 
